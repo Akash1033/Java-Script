6fe973dc26023394e6ce9c3eec4576aa65999944
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-05 06:12:41 +0000

    js updated

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code asnewer version
+
+// alert ("hello")
+// alert (2+2)
+
+console.log(3+3)
+console.log("Akash")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false 
+let state = null;
+
+// ****************Primitive Datatypes************
+//number => 2 to the power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol=>  unique
+
+//object
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
