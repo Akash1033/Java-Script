26b538747c7b4ed63cc89dfe422ee7e9937164f2
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-05 06:43:56 +0000

    js updated

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = null 
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33"=> 33
+//"33abc" => NaN
+//tru
