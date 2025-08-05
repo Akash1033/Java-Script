0aea7ac7f61081ab056cb884dffbbfa4fbea7d94
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-05 05:21:49 +0000

    js updated

---
+++ 01_Basics/01_Variable.js
@@ -0,0 +1,13 @@
+const accountId = 132454
+let accountEmail = "akashvohra@gmail.com"
+var accountPasssword = "123456"
+accountCity  = "Ludhiana"
+let accountState;
+
+// accountId = 2
+accountEmail = "abc@gmail"
+accountPasssword = "13466"
+accountCity = "Hoshiarpur"
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPasssword, accountCity, accountState])
