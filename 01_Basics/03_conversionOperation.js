29193de0bac675394be424439cc214d53950c637
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-06 04:08:36 +0000

    js updated

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -8,4 +8,18 @@ console.log(valueInNumber)
 
 // "33"=> 33
 //"33abc" => NaN
-//tru
+//true => 1 ; false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+// "" =>false
+// "Akash" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof someNumber)
