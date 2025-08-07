e7bc0bf1497bc242de60d654ed7d421f4dc569b8
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-07 04:28:17 +0000

    js updated

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = null 
 
-console.log(typeof score)
+// console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 // "33"=> 33
 //"33abc" => NaN
@@ -19,7 +19,44 @@ console.log(booleanIsloggedIn);
 // "" =>false
 // "Akash" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof someNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof someNumber)
+
+
+//*************************Operations ******************* */
+
+let value = 3
+let negValue  = -value 
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+
+
+let str1 = "hello"
+let str2 = " Akash"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+ 2)
+console.log(2+ 2 + "2")
+
+console.log(3+4 * 5%3);
+
+console.log(+true)
+console.log(+false)
+console.log(+"")
+
+
+let num1 , num2 , num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter= 120
+gamecounter++;
+++gamecounter;
+console.log(gamecounter)
