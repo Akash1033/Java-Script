fad2e767b7a9b2184e346ad89a2835779631f2b3
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-08 16:13:24 +0000

    js updated

--- 01_Basics/05_Datatype-Summary.js
+++ 01_Basics/05_Datatype-Summary.js
@@ -39,3 +39,35 @@ const scoreValue  = 20.0
      }
 
      myFunction()
+
+
+
+     // +++++++++++++++++++++++++++ MEMORY ++++++++++++++++++++++++++++++++
+     
+     // Stack  And Heap Memory
+
+     // stack(Primitive)
+     // Heap (Non-Primitive)
+
+     let myYoutubename = "worldlyWonders"
+
+     let anotherName = myYoutubename
+     anotherName = "AkashVlogs"
+
+     console.log(anotherName);
+     console.log(myYoutubename);
+     
+
+     let userOne = {
+        email : "user@gcel.com",
+        upi : "abc"
+     }
+
+     let userTwo = userOne
+
+     userTwo.email = "Akash@a.com"
+
+    console.log(userOne.email);
+    console.log(userTwo.email);
+    
+     
