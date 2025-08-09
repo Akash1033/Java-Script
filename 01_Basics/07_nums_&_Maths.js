14df16320b649b2a3053f52cce9ee5284047ed32
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-09 10:24:46 +0000

    js updated

---
+++ 01_Basics/07_nums_&_Maths.js
@@ -0,0 +1,45 @@
+const score = 400
+
+const balance = new Number(500)
+// console.log(balance);
+// console.log(score);
+
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// console.log(typeof balance);
+
+
+// const otherNumber = 23.959
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++ Maths +++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,5,9,7,45));
+// console.log(Math.max(4,5,9,7,45));
+
+console.log(Math.random());
+console.log((Math.random() * 10 ) +1);
+console.log(Math.floor(Math.random() * 10 ) +1);
+
+
+
+const min = 10
+const max  = 15
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
