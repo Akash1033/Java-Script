bb29929fbc787e6bda3553401356221eecad0dcd
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-10 16:27:14 +0000

    js updated

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,16 @@
+ // Immediately Invoked Function Expression (IIFE)
+
+( function chai(){
+    // named IIFE
+    console.log(`DB Connected`);
+    
+ })();
+
+ (() => {
+    console.log('DB CONNECTED');
+    
+ })();
+
+ 
+
+ 
