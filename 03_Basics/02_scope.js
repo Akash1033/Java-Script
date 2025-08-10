a3181463d1aea70a2722b3d323ac0292c3c0c44a
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-10 15:55:32 +0000

    js updated

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,59 @@
+// var c =300
+
+let a = 300
+if (true) {
+   let a = 10
+   const b = 20
+    // console.log(a);
+    
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username  = "Akash"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+
+if (true) {
+    const username = "Akash"
+    if(username=== "Akash"){
+       const website = " youtube" 
+    //    console.log(username + website);
+       
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++ Interesting +++++++++++++++
+console.log(addone(10));
+
+
+function addone(value){
+    return value + 1
+}
+
+
+const addTwo = function(value){
+    return value +2
+}
+console.log(addTwo(5));
+
