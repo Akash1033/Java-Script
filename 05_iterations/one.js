cc273d132faa5d11e04bb62da12bb06c4179fc7c
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-11 16:07:16 +0000

    js updated

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+// for loop
+
+// for (let index = 0; index<10; index++) {
+//     const element = index;
+//     if (element == 5) {
+//         console.log(`5 is best number`);
+        
+//     }
+//     console.log(element);
+// }
+
+for (let index = 0; index < 10; index++) {
+    console.log(`outer loop value :${index}`);
+    for (let j = 0; j < 10; j++) {
+        // console.log(`Inner loop value is ${j} and outer is ${index}`);
+        // console.log(index + '*' + j + '=' + index*j );
+        
+    }
+
+    
+}
