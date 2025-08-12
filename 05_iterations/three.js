ba875f42e2662a8947a689cfcebdd99c74a9fbad
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-12 16:50:44 +0000

    js updated

---
+++ 05_iterations/three.js
@@ -0,0 +1,28 @@
+// for of 
+// Array specific loops
+
+// ["","",""]
+// [{},{},{}]
+
+
+const arr = [1,2,3,4,5]
+
+for (const i of arr) {
+    console.log(i);
+    
+}
+
+const greeting = "Hello World"
+
+for (const num of greeting) {
+    // console.log(`greet is ${num}`);
+    
+}
+
+// Maps
+const map = new Map()
+map.set('IN', "india")
+map.set('USA', "United States of america")
+
+
+console.log(map);
