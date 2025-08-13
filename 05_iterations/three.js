862c206e39dacf1a5f8f94020ec18256c99e9c16
Akash Vohra <109934173+Akash1033@users.noreply.github.com>
2025-08-13 04:10:38 +0000

    js updated

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -25,4 +25,18 @@ map.set('IN', "india")
 map.set('USA', "United States of america")
 
 
-console.log(map);
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ":-" ,value);  
+}
+
+const myObject = {
+    game1 : 'PUBG',
+    'game2' : 'FREE FIRE'
+}
+
+// for (const [key , value] of myObject) {
+//     console.log(key , ":-" , value);  
+// }
+
